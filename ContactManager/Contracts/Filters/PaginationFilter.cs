2a4b9273425412b306737dcc1248945b11e79ab8
Mohamed Ali <128579563+MohamedAliTera@users.noreply.github.com>
2025-03-30 00:16:23 +0200

    Add pagination helpers (filter and result)

---
+++ ContactManager/Contracts/Filters/PaginationFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace ContactManager.Contracts.Filters;
+
+public class PaginationFilter
+{
+    private const int maxPageSize = 10;
+    private int pageSize = 5;
+
+    public int PageNumber { get; set; } = 1;
+    public int PageSize
+    {
+        get => pageSize;
+        set => pageSize = value > maxPageSize ? maxPageSize : value;
+    }
+}
