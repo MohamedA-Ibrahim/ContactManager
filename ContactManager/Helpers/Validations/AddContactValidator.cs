8ad68deae440d2ab63777449395299817a6234e2
Mohamed Ali <128579563+MohamedAliTera@users.noreply.github.com>
2025-03-29 23:41:24 +0200

    Improve phone number validation

--- ContactManager/Helpers/Validations/AddContactValidator.cs
+++ ContactManager/Helpers/Validations/AddContactValidator.cs
@@ -8,7 +8,7 @@ public class AddContactValidator : AbstractValidator<AddContactRequest>
     public AddContactValidator()
     {
         RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
-        RuleFor(x => x.Phone).NotEmpty().MaximumLength(20);
+        RuleFor(x => x.Phone).NotEmpty().EgyptianPhoneNumber();
         RuleFor(x => x.Address).NotEmpty().MaximumLength(100);
         RuleFor(x => x.Notes).MaximumLength(500);
     }
---
+++ ContactManager/Helpers/Validations/ValidationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace ContactManager.Helpers.Validations;
+
+public static class ValidationExtensions
+{
+    public static IRuleBuilderOptions<T, string> EgyptianPhoneNumber<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .Matches("^01[0-2,5][0-9]{8}$")
+            .WithMessage("Invalid Egyptian phone number format. It must be 11 digits starting with 010, 011, 012, or 015.");
+    }
+}
